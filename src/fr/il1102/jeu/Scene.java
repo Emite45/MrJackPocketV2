01433bb9e8128a6007a6bcc37841a6e8625c4089
Aymeric L <thesuperspeed45@gmail>
2021-01-31 18:38:07 +0100

    écran d'accueil toujours

--- src/fr/il1102/jeu/Scene.java
+++ src/fr/il1102/jeu/Scene.java
@@ -1015,10 +1015,10 @@ public class Scene extends JPanel {
 			g2.drawString("Bienvenue sur le Jeu Mr Jack Pocket : ", 350, 150);
 			g2.setFont(police2);
 			g2.drawString("Pour commencer à vous faufiler dans les ruelles de Londres : ", 350, 750);	
-			g2.drawString("Appuyez sur ENTRER", 650, 680);
+			g2.drawString("Appuyez sur ENTRER", 700, 780);
 
 			g2.setFont(police3);
-			g2.drawString("Elias Dève    Aymeric Lefebvre    Léonie Petit", 650, 800);
+			g2.drawString("Elias Dève    Aymeric Lefebvre    Léonie Petit", 630, 850);
 			
 		} else if (ecranAlibi == true && ecranAccueil == false ){  //Ecran du Menu pour Mr Jack
 			
